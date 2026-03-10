18b584bd526b342d4469cc99de8f9ca65266b5a2
Jeffrin <merinjeffrin0@gmail.com>
2026-03-10 20:10:59 +0530

    Exports command_exists, command_runs

--- devsetup/system/__init__.py
+++ devsetup/system/__init__.py
@@ -3,8 +3,10 @@ from devsetup.system.package_manager_detector import (
     get_package_manager,
     is_apt, is_dnf, is_pacman, is_brew, is_winget,
 )
+from devsetup.system.command_detector import command_exists, command_runs
 
 __all__ = [
     "get_os", "is_linux", "is_macos", "is_windows",
     "get_package_manager", "is_apt", "is_dnf", "is_pacman", "is_brew", "is_winget",
+    "command_exists", "command_runs",
 ]
