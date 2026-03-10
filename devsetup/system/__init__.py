ac52997dd6d3b3eae786b99b4d142785c82970f5
Jeffrin <merinjeffrin0@gmail.com>
2026-03-10 16:51:24 +0530

    Add package manager detection to __all__ exports

--- devsetup/system/__init__.py
+++ devsetup/system/__init__.py
@@ -1,3 +1,10 @@
 from devsetup.system.os_detector import get_os, is_linux, is_macos, is_windows
+from devsetup.system.package_manager_detector import (
+    get_package_manager,
+    is_apt, is_dnf, is_pacman, is_brew, is_winget,
+)
 
-__all__ = ["get_os", "is_linux", "is_macos", "is_windows"]
+__all__ = [
+    "get_os", "is_linux", "is_macos", "is_windows",
+    "get_package_manager", "is_apt", "is_dnf", "is_pacman", "is_brew", "is_winget",
+]
