1f3dadfa4cc1dda85c2c1d8ebd7d09e05dd8595f
Jeffrin <merinjeffrin0@gmail.com>
2026-03-13 09:22:16 +0530

    Update __init__.py

--- devsetup/utils/__init__.py
+++ devsetup/utils/__init__.py
@@ -1,3 +1,10 @@
-from devsetup.utils.logger import info, error, success, warn, check, skip, install, fail, debug
+from devsetup.utils.logger import (
+    info, error, success, warn, check, skip, install, fail, debug, version_log,
+)
+from devsetup.utils.version_parser import parse_version
 
-__all__ = ["info", "error", "success", "warn", "check", "skip", "install", "fail", "debug"]
+__all__ = [
+    "info", "error", "success", "warn", "check",
+    "skip", "install", "fail", "debug", "version_log",
+    "parse_version",
+]
